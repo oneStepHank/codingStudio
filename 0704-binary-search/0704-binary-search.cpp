aa116fca2b232eebca8c1034cc396c492c79e270
한결 <94732149+oneStepHank@users.noreply.github.com>
2025-07-21 18:44:20 +0900

    Time: 0 ms (100%), Space: 31.3 MB (45.33%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int lo = 0 , hi = nums.size() - 1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2; // calculate middle index in current boundary
+            if(nums[mid] < target){
+                lo = mid + 1;
+            }else if( nums[mid] > target){
+                hi = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
